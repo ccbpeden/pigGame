fb061f6e21d0b3fb7085278ed5be80325831ab66
charles peden and jeremiah freeman <jeremiah@>
2017-01-25 15:14:16 -0800

    complete multi-dice functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
-var tempScore = 0;
+var tempScore;
 var user0 = {userName: "Player 1", userScore: 0};
 var user1 = {userName: "Player 2", userScore: 0};
 var rolls = [];
-var currentUser = "Player 1";
-var currentScore = 0;
-var input = true;
-var hold = true;
+var currentUser;
+var currentScore;
+// var hold = true;
+var numberOfDice = 1;
+var endTurn;
+
+var clearGame = function(){
+  user0.userScore = 0;
+  user1.userScore = 0;
+  tempScore = 0;
+  currentUser = "Player 1";
+  rolls = [];
+  currentScore = 0;
+  endTurn = false;
+  $("#player1score").text(user0.userScore);
+  $("#player2score").text(user1.userScore);
+  $("#current-user").text(currentUser);
+  $(".victory").hide();
+  $("#temp-roll").empty();
+  $("#temp-score").empty();
+}
 
 var variableDice = function(input){
+  endTurn = false;
+  rolls = [];
   for (var i = 0; i < input; i += 1){
     var number = parseInt(1 + Math.floor(Math.random() * 6));
     if (number === 1){
@@ -24,32 +43,40 @@ var variableDice = function(input){
   	console.log("rolls[i]")
  	 if (rolls[i] === 1){
  	 	tempScore = 0;
+    endTurn = true;
 	  };
   };
   console.log(tempScore);
 };
 
-// var diceRoll = function(){ //simulates RNG 1-6
-//   var number = parseInt(1 + Math.floor(Math.random() * 6));
-//   if (number === 1){
-//     tempScore = 0;
-//     return number;
-//   } else {
-//     tempScore = tempScore + number;
-//     return number;
-//   };
-// };
+var printRolls = function(){
+    $("#temp-roll").empty();
+  for(i = 0; i < rolls.length; i++){
+    var dieNumber = rolls[i];
+  $("#temp-roll").append(dieNumber + ", ");
+  };
+}
 
 var outputScore = function(){
   if (currentUser === user0.userName){
     return "#player1score";
   } else {
     return "#player2score";
-  }
+  };
 }
 
+var victoryCheck = function(){
+  var scoreTotal = currentScore + tempScore
+  if (scoreTotal >= 100){
+    //victory action
+    $("#winner-name").text(currentUser);
+    $(".victory").slideDown();
+
+  };
+};
+
 var turnDetermine = function(input){
-  if (input === 1){
+  if (endTurn){
     if (currentUser === user0.userName){
     console.log("input determined false")
       currentUser = user1.userName;
@@ -78,30 +105,21 @@ var userHold = function() {
   };
 };
 
-
-
-var victoryCheck = function(){
-  var scoreTotal = currentScore + tempScore
-  if (scoreTotal >= 100){
-    alert("winner winner")
-    //victory action
-    $(".victory").slideDown();
-  };
-};
-
 //frontendery
 
 $(document).ready(function() {
-  $("#player1score").empty().text(user0.userScore);
-  $("#player2score").empty().text(user1.userScore);
-  $("#current-user").empty().text(currentUser);
+  clearGame();
+  $("#dice-submit").click(function() {
+    numberOfDice = $("#dice-control").val();
+    clearGame();
+  });
 
   $("#roll-button").click(function() {
-    variableDice();
-    $("#temp-roll").empty().text(rolls);
+    variableDice(numberOfDice);
+    printRolls();
     $("#temp-score").empty().text(tempScore);
     victoryCheck();
-    turnDetermine(tempRoll);
+    turnDetermine();
     $("#current-user").empty().text(currentUser);
 
   });
@@ -115,4 +133,8 @@ $(document).ready(function() {
     $("#temp-roll").empty();
     $("#temp-score").empty();
   });
+
+    $("#reset-button").click(function(){
+      clearGame();
+    });
 });
